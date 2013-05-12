537b7c74ee5648cf573d64b8c2552010e9bfbac7
silverforge <janos.murvaigaal@gmail.com>
2013-05-12 17:29:10 +0200

    mgj : modern ui views added

---
+++ SilverForge.CeasarCipher/CaliburnContentLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using FirstFloor.ModernUI.Windows;
+
+namespace SilverForge.CeasarCipher
+{
+	public class CaliburnContentLoader : DefaultContentLoader
+	{
+		protected override object LoadContent(Uri uri)
+		{
+			var content = base.LoadContent(uri);
+			if (content == null)
+				return content;
+
+			var vm = Caliburn.Micro.ViewModelLocator.LocateForView(content);
+			if (vm == null)
+				return content;
+
+			if (content is DependencyObject)
+			{
+				Caliburn.Micro.ViewModelBinder.Bind(vm, content as DependencyObject, null);
+			}
+			return content;
+		}
+	}
+}
---
+++ SilverForge.CeasarCipher/ViewModels/DecoderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+using SilverForge.CeasarCipher.Interfaces;
+
+namespace SilverForge.CeasarCipher.ViewModels
+{
+	[Export]
+	public class DecoderViewModel : Screen, IViewModel
+	{
+		
+	}
+}
