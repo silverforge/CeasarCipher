3c6536a6aa99acf7c881cbc9baa6f7975967dd79
silverforge <janos.murvaigaal@gmail.com>
2013-05-12 21:24:04 +0200

    mgj : views has been extended

--- SilverForge.CeasarCipher/ViewModels/DecoderViewModel.cs
+++ SilverForge.CeasarCipher/ViewModels/DecoderViewModel.cs
@@ -8,14 +8,38 @@ namespace SilverForge.CeasarCipher.ViewModels
 	public class DecoderViewModel : Screen, IViewModel
 	{
 		private string _password;
+		private string _text;
+
 		public string Password
 		{
 			get { return _password; }
 			set
 			{
+				if (value == _password) return;
 				_password = value;
 				NotifyOfPropertyChange(() => Password);
 			}
+		}
+
+		public string Text
+		{
+			get { return _text; }
+			set
+			{
+				if (value == _text) return;
+				_text = value;
+				NotifyOfPropertyChange(() => Text);
+			}
+		}
+
+		public void Decode()
+		{
+			
+		}
+
+		public void Reset()
+		{
+			
 		}
 	}
 }
--- SilverForge.CeasarCipher/ViewModels/EncoderViewModel.cs
+++ SilverForge.CeasarCipher/ViewModels/EncoderViewModel.cs
@@ -7,6 +7,39 @@ namespace SilverForge.CeasarCipher.ViewModels
 	[Export]
 	public class EncoderViewModel : Screen, IViewModel
 	{
-		
+		private string _password;
+		private string _text;
+
+		public string Password
+		{
+			get { return _password; }
+			set
+			{
+				if (value == _password) return;
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+			}
+		}
+
+		public string Text
+		{
+			get { return _text; }
+			set
+			{
+				if (value == _text) return;
+				_text = value;
+				NotifyOfPropertyChange(() => Text);
+			}
+		}
+
+		public void Encode()
+		{
+
+		}
+
+		public void Reset()
+		{
+
+		}
 	}
 }
